1e40d7592756cb205fe63a446039a0b820f8f8f9
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-05-09 03:50:34 +0300

    Added routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import Users from './users'
+import Contact from './contact'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const routing = (
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  )
+  ReactDOM.render(routing, document.getElementById('root'))
